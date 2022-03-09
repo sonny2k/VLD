ec0f4b4ccd72fc967b45f18f3844fcdd0d3a5e5c
TTL <thanh.187pm14010@vanlanguni.vn>
2022-03-10 02:19:55 +0700

    ádasdas

--- server/routes/user/account.js
+++ server/routes/user/account.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../../middleware/auth");
-const imgur = require('imgur');
-const fs = require('fs');
-const fileUpload = require('express-fileupload');
-router.use(fileUpload());
 
 const Account = require("../../models/Account");
 
