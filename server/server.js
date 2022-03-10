0b9b3823cbe627637118dedad6732b0ff2c9615f
TTL <thanh.187pm14010@vanlanguni.vn>
2022-03-10 16:32:25 +0700

    fix cors issues

--- server/server.js
+++ server/server.js
@@ -37,6 +37,12 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 //User routes
 app.use("/api/user/auth", authUserRouter);
 app.use("/api/user", userRouter);
