38590ed224cdfe22b6365b093ab61f9cf2dc1710
TTL <thanh.187pm14010@vanlanguni.vn>
2022-03-12 23:26:18 +0700

    ssaas

--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 
 //User routes
