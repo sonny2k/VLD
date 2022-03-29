e5ab812b05544230289e39a1beca5f5c99e43ccb
tankhanh <dinhtankhanh14@gmail.com>
2022-03-29 12:57:12 +0700

    thêm xóa sửa product

---
+++ server/models/Product.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProductShema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    categories: {
+      type: Array,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("products", ProductShema);
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const userRouter = require("./routes/user/user");
 const accountUserRouter = require("./routes/user/account");
 const homepageRouter = require("./routes/user/homepage");
 const consultationRouter = require("./routes/user/consultation");
+const productRouter = require("./routes/admin/product");
 
 const connectDB = async () => {
   try {
@@ -32,9 +33,10 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express());
-app.use(express.json({limit: '50mb', extended: true}));
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 
 //User routes
 app.use("/api/user/auth", authUserRouter);
@@ -42,6 +44,7 @@ app.use("/api/user", userRouter);
 app.use("/api/user/account", accountUserRouter);
 app.use("/api/home", homepageRouter);
 app.use("/api/user/consultation", consultationRouter);
+app.use("/api/admin/product", productRouter);
 
 app.get("/", (req, res) => res.send("VAN LANG DOCTOR SERVER OF TEAM 16"));
 
