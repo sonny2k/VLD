d3e4777faa303b95d15f80425fa5f60a242a81b4
TTL <thanh.187pm14010@vanlanguni.vn>
2022-04-15 14:29:14 +0700

    fix prescription detail

--- server/routes/user/prescription.js
+++ server/routes/user/prescription.js
@@ -2,15 +2,20 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../../middleware/auth");
 const Prescription = require("../../models/Prescription");
+const Consultation = require("../../models/Consultation");
 
 router.get("/viewPrescriptionByConsultation", verifyToken, async (req, res) => {
   const {
     id,
   } = req.body;
   try {
-    var populateQuery = ({path:'medicines.product'});
-    const prescription = await Prescription.findOne({ consultation: id }).populate(populateQuery);
-    res.json(prescription);
+    var populateQueryP = ({path:'medicines.product'});
+    var populateQueryCD = ({path:'doctor', populate: {path:'account'}});
+    var populateQueryCU = ({path:'user', populate: {path:'account'}});
+    const docinfo = await Consultation.find({ _id: id }).populate(populateQueryCD);
+    const userinfo = await Consultation.find({ _id: id }).populate(populateQueryCU);
+    const prescription = await Prescription.findOne({ consultation: id }).populate(populateQueryP);
+    res.json({prescription, docinfo, userinfo});
   } catch (error) {
     console.log(error);
     res.status(500).json({
