4ce4e1d09536617a73d608e2ce18730b02afa789
TTL <thanh.187pm14010@vanlanguni.vn>
2022-04-26 14:38:58 +0700

    fix get all product api

--- server/routes/admin/product.js
+++ server/routes/admin/product.js
@@ -49,25 +49,11 @@ router.get("/find/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //GET ALL PRODUCTS
 router.get("/viewProduct", async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
   try {
-    let products;
-
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
-    } else if (qCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [qCategory],
-        },
-      });
-    } else {
-      products = await Product.find();
-    }
-
+    const products = await Product.find();
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json(err);
