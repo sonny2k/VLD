e4ccef984f4aca6bcb279ec218edf8ca1d034db2
TTL <thanh.187pm14010@vanlanguni.vn>
2022-05-04 14:40:41 +0700

    fix api update doctor detail

--- server/routes/doctor/account.js
+++ server/routes/doctor/account.js
@@ -53,4 +53,53 @@ router.post("/workingtime", verifyToken, async (req, res) => {
   }
 });
 
+router.put("/detailinfo", verifyToken, async (req, res) => {
+  const {
+    educationplace,
+    workcertificate,
+    level,
+    degree,
+    description,
+    excellence,
+    workhistory,
+    education,
+  } = req.body;
+
+  try {
+    let updatedAccount = {
+      educationplace,
+      workcertificate,
+      level,
+      degree,
+      description,
+      excellence,
+      workhistory,
+      education,
+    };
+
+    const profileupdatecondition = { account: req.accountId };
+    updatedAccount = await Doctor.findOneAndUpdate(
+      profileupdatecondition,
+      updatedAccount,
+      { new: true }
+    );
+
+    // User not authorized to update profile
+    if (!updatedAccount)
+      return res.status(400).json({
+        success: false,
+        message: "Người dùng không có quyền cập nhật tài khoản này",
+      });
+
+    res.json({
+      success: true,
+      message: "Cập nhật chi tiết tài khoản thành công",
+      account: updatedAccount,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Lỗi nội bộ" });
+  }
+});
+
 module.exports = router;
