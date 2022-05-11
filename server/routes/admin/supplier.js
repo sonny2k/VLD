e13f3b4ec3975e3389e4c33f65cdf940a4bd5a8a
tankhanh <dinhtankhanh14@gmail.com>
2022-05-11 15:41:18 +0700

    api suppliers

--- server/routes/admin/supplier.js
+++ server/routes/admin/supplier.js
@@ -13,4 +13,98 @@ router.get("/viewSupplier", async (req, res) => {
   }
 });
 
+//CREATE SUPPLIER
+router.post("/createSupplier", verifyToken, async (req, res) => {
+  const { name, description } = req.body;
+  try {
+    const newSup = new Supplier({
+      name,
+      description,
+    });
+    await newSup.save();
+    res.json({
+      success: true,
+      message: "Bạn đã tạo nhà cung cấp thành công",
+      newSup,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Lỗi tải dữ liệu",
+    });
+  }
+});
+
+//UPDATE SUPPLIER
+router.put("/updateSupplier/:id", verifyToken, async (req, res) => {
+  const { name, description } = req.body;
+  try {
+    let updateSup = {
+      name,
+      description,
+    };
+    const SupupdateCondition = {
+      _id: req.params.id,
+    };
+    upSup = await Supplier.findOneAndUpdate(SupupdateCondition, updateSup, {
+      new: true,
+    });
+    if (!upSup)
+      return res.status(400).json({
+        success: false,
+        message: "Không có quyền cập nhật nhà cung cấp",
+      });
+    res.json({
+      success: true,
+      message: "Bạn đã cập nhật nhà cung cấp thành công",
+      supplier: upSup,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Lỗi tải dữ liệu",
+    });
+  }
+});
+
+//DELETE SUPPLIER
+router.delete("/deleteSupplier/:id", verifyToken, async (req, res) => {
+  try {
+    deSup = await Supplier.findOneAndDelete({ _id: req.params.id });
+    if (!deSup)
+      return res
+        .status(400)
+        .json({ success: false, message: "Không có quyền xóa nhà cung cấp" });
+    res.json({
+      success: true,
+      message: "Xóa nhà cung cấp thành công",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Lỗi tải dữ liệu",
+    });
+  }
+});
+
+//SEACRH SUPPLIER
+router.get("/searchSupplier/:name", verifyToken, async (req, res) => {
+  try {
+    var keywordSup = new RegExp(req.params.name, "i");
+    console.log(`${keywordSup}`);
+    const findName = await Supplier.find({ name: keywordSup });
+    if (!keywordSup) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Không tìm thấy nhà cung cấp" });
+    }
+    res.send(findName);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Lỗi tìm kiếm" });
+  }
+});
 module.exports = router;
