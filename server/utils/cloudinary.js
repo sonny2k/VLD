62a567e3339fe5ce70ade42c8e55bafd27c0a243
TTL <thanh.187pm14010@vanlanguni.vn>
2022-06-03 09:29:45 +0700

    fix api upload pic

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dm92pmz6s",
+  api_key: "872796819671222",
+  api_secret: "x6GUosbavFf4NYMmYgjJUOJn2u4",
 });
 
 module.exports = { cloudinary };
